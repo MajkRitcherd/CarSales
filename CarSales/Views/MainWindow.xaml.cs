b15ff5883d2108210e670431420147834fa83691
MajkRitcherd <misutak87@gmail.com>
2026-05-29 13:39:56 +0200

    Move MainWindow to Views to follow MVVM architecture

--- CarSales/Views/MainWindow.xaml.cs
+++ CarSales/Views/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CarSales
+namespace CarSales.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
