4e572b07d47aaf8afc48becfac829b04803c83b0
MajkRitcherd <misutak87@gmail.com>
2026-06-02 14:42:32 +0200

    Move logic from MainWindow to MainWindowViewModel to follow MVVM architecture

---
+++ CarSales/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using CarSales.Models;
+using CarSales.Services;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Win32;
+
+namespace CarSales.ViewModels
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        private readonly FileService _fileService = new();
+
+        [ObservableProperty]
+        private double _averageNetPrice;
+
+        [ObservableProperty]
+        private string _newManufacturer = string.Empty;
+
+        [ObservableProperty]
+        private string _newModel = string.Empty;
+
+        [ObservableProperty]
+        private string _newNetPrice = string.Empty;
+
+        [ObservableProperty]
+        private DateTime? _newSoldOn = DateTime.Today;
+
+        [ObservableProperty]
+        private string _newVatPercent = string.Empty;
+
+        [ObservableProperty]
+        private string _openedFilePath = "Žádný soubor není načten";
+
+        private SalesData? _salesData;
+
+        [ObservableProperty]
+        private string _searchText = string.Empty;
+
+        [ObservableProperty]
+        private int _selectedTabIndex;
+
+        [ObservableProperty]
+        private double _totalRevenue;
+
+        [ObservableProperty]
+        private int _totalVehiclesCount;
+
+        public MainWindowViewModel()
+        {
+            UpdateView();
+        }
+
+        /// <summary>
+        /// Gets collection of vehicle row view models.
+        /// </summary>
+        public ObservableCollection<VehicleRowViewModel> VehicleRowViewModels { get; } = [];
+
+        /// <summary>
+        /// Gets collection of vehicle row view models that were sold during weekend.
+        /// </summary>
+        public ObservableCollection<WeekendVehicleRowSummaryViewModel> WeekendVehicleRowSummaryViewModels { get; } = [];
+
+        [RelayCommand]
+        private void AddVehicleRecord()
+        {
+            double netPrice = 0d, vatPercent = 0d;
+            try
+            {
+                if (!double.TryParse(NewNetPrice, out netPrice))
+                    throw new ArgumentException($"Failed to parse Net price '{NewNetPrice}' to double.");
+
+                if (!double.TryParse(NewVatPercent, out vatPercent))
+                    throw new ArgumentException($"Failed to parse Vat percent '{NewVatPercent}' to double.");
+            }
+            catch (ArgumentException ex)
+            {
+                MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            var vehicle = new Vehicle()
+            {
+                ModelName = NewModel,
+                NetPrice = netPrice,
+                VatPercent = vatPercent,
+                SoldOn = NewSoldOn,
+            };
+
+            var manufacturer = _salesData?.Manufacturers
+                .FirstOrDefault(m => m.Name.Equals(NewManufacturer, StringComparison.OrdinalIgnoreCase));
+
+            if (manufacturer == null)
+            {
+                manufacturer = new Manufacturer() { Name = NewManufacturer };
+                _salesData?.Manufacturers.Add(manufacturer);
+            }
+
+            manufacturer.Vehicles.Add(vehicle);
+
+            UpdateView();
+        }
+
+        [RelayCommand]
+        private void LoadSalesData()
+        {
+            var openFileDialog = new OpenFileDialog
+            {
+                Filter = "Podporované soubory (*.xml;*.csv)|*.xml;*.csv|XML soubory (*.xml)|*.xml|CSV soubory (*.csv)|*.csv",
+                Title = "Vyberte soubor s prodeji aut"
+            };
+
+            // Return when failed to open dialog
+            if (openFileDialog.ShowDialog() == false)
+                return;
+
+            var selectedFilePath = openFileDialog.FileName;
+            try
+            {
+                _salesData = _fileService.LoadSalesData(selectedFilePath);
+                OpenedFilePath = selectedFilePath;
+                UpdateView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        partial void OnSearchTextChanged(string value)
+        {
+            UpdateView();
+        }
+
+        partial void OnSelectedTabIndexChanged(int value)
+        {
+            RecalculateStatistics();
+        }
+
+        /// <summary>
+        /// Recalculates statistics.
+        /// </summary>
+        private void RecalculateStatistics()
+        {
+            if (VehicleRowViewModels == null || !VehicleRowViewModels.Any())
+                return;
+
+            int vehicleCount;
+            double totalRevenue, averageNetPrice;
+            if (SelectedTabIndex == 0) // All vehicles
+            {
+                vehicleCount = VehicleRowViewModels.Count;
+                totalRevenue = VehicleRowViewModels.Sum(x => x.Vehicle.GrossPrice);
+                averageNetPrice = vehicleCount > 0
+                    ? VehicleRowViewModels.Average(x => x.Vehicle.NetPrice)
+                    : 0;
+            }
+            else // Weekend vehicles
+            {
+                vehicleCount = WeekendVehicleRowSummaryViewModels.Count;
+                totalRevenue = WeekendVehicleRowSummaryViewModels.Sum(x => x.TotalGrossPrice);
+                averageNetPrice = vehicleCount > 0
+                    ? WeekendVehicleRowSummaryViewModels.Sum(x => x.TotalNetPrice) / vehicleCount
+                    : 0;
+            }
+
+            TotalVehiclesCount = vehicleCount;
+            TotalRevenue = totalRevenue;
+            AverageNetPrice = averageNetPrice;
+        }
+
+        [RelayCommand]
+        private void SaveSalesData()
+        {
+            if (_salesData == null || !_salesData.Manufacturers.Any())
+            {
+                MessageBox.Show("No data to be saved", "Warning", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+
+            var saveFileDialog = new SaveFileDialog
+            {
+                Filter = "XML file (*.xml)|*.xml|CSV file (*.csv)|*.csv",
+                Title = "Save sales data",
+                FileName = "SalesData_Export"
+            };
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                try
+                {
+                    var filePath = saveFileDialog.FileName;
+
+                    _fileService.SaveSalesData(filePath, _salesData);
+
+                    var messageBoxMessage = "Data were sucessfully saved to {0} file";
+                    if (string.Equals(FileService.GetFileExtension(filePath), ".xml", StringComparison.OrdinalIgnoreCase))
+                    {
+                        messageBoxMessage = string.Format(messageBoxMessage, "XML");
+                    }
+                    else
+                    {
+                        messageBoxMessage = string.Format(messageBoxMessage, "CSV");
+                    }
+
+                    MessageBox.Show(messageBoxMessage, "Saved", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Exception thrown", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Updates grid data.
+        /// </summary>
+        private void UpdateGridData()
+        {
+            if (_salesData == null)
+                return;
+
+            var filter = SearchText.Trim().ToLowerInvariant();
+
+            VehicleRowViewModels.Clear();
+            foreach (var manufacturer in _salesData.Manufacturers)
+            {
+                foreach (var vehicle in manufacturer.Vehicles)
+                {
+                    var row = new VehicleRowViewModel(manufacturer.Name, vehicle);
+
+                    if (!string.IsNullOrEmpty(filter))
+                    {
+                        bool matchesManufacturerName = manufacturer.Name.Contains(filter, StringComparison.InvariantCultureIgnoreCase);
+                        bool matchesModelName = vehicle.ModelName.Contains(filter, StringComparison.InvariantCultureIgnoreCase);
+
+                        if (!matchesManufacturerName && !matchesModelName)
+                        {
+                            continue;
+                        }
+                    }
+
+                    VehicleRowViewModels.Add(row);
+                }
+            }
+
+            var weekendSummaryModels = VehicleRowViewModels
+                .Where(x => x.Vehicle.SoldOn.HasValue
+                         && (x.Vehicle.SoldOn.Value.DayOfWeek == DayOfWeek.Saturday || x.Vehicle.SoldOn.Value.DayOfWeek == DayOfWeek.Sunday))
+                .GroupBy(x => new { x.ManufacturerName, x.Vehicle.ModelName })
+                .Select(group => new WeekendVehicleRowSummaryViewModel()
+                {
+                    VehicleManufacturerName = group.Key.ManufacturerName,
+                    VehicleModelName = group.Key.ModelName,
+                    TotalVehiclesSold = group.Count(),
+                    TotalNetPrice = group.Sum(x => x.Vehicle.NetPrice),
+                    TotalGrossPrice = group.Sum(x => x.Vehicle.GrossPrice),
+                });
+
+            WeekendVehicleRowSummaryViewModels.Clear();
+            foreach (var weekendSummary in weekendSummaryModels)
+            {
+                WeekendVehicleRowSummaryViewModels.Add(weekendSummary);
+            }
+        }
+
+        /// <summary>
+        /// Updates a view.
+        /// </summary>
+        private void UpdateView()
+        {
+            UpdateGridData();
+            RecalculateStatistics();
+        }
+    }
+}
