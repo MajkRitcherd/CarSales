581889600cbc9da2edc0dc931b14e8f5baf553f3
MajkRitcherd <misutak87@gmail.com>
2026-06-02 09:09:35 +0200

    Use czech culture to correctly display prices in CZK

--- CarSales/App.xaml.cs
+++ CarSales/App.xaml.cs
@@ -1,6 +1,6 @@
-﻿using System.Configuration;
-using System.Data;
+﻿using System.Globalization;
 using System.Windows;
+using System.Windows.Markup;
 
 namespace CarSales
 {
@@ -9,6 +9,21 @@ namespace CarSales
     /// </summary>
     public partial class App : Application
     {
+        /// <inheritdoc/>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var czechCulture = new CultureInfo("cs-CZ");
+
+            CultureInfo.CurrentCulture = czechCulture;
+            CultureInfo.CurrentUICulture = czechCulture;
+
+            FrameworkElement.LanguageProperty.OverrideMetadata(
+                typeof(FrameworkElement),
+                new FrameworkPropertyMetadata(XmlLanguage.GetLanguage("cs-CZ"))
+            );
+
+            base.OnStartup(e);
+        }
     }
 
 }
